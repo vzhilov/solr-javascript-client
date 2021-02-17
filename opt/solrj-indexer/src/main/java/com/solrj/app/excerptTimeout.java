d1e9a9fe063e65b3b195668d7ecaf894a281ba28
vzhilov <vzhilov@gmail.com>
2021-02-17 14:28:26 +0300

    Dev Update: thread timout

--- opt/solrj-indexer/src/main/java/com/solrj/app/excerptTimeout.java
+++ opt/solrj-indexer/src/main/java/com/solrj/app/excerptTimeout.java
@@ -1,3 +1,14 @@
+    /**
+     * Configures SolrClient parameters and returns a SolrClient instance.
+     * 
+     * @return a SolrClient instance
+     */
+    private static SolrClient getSolrClient() {
+        return new HttpSolrClient.Builder(SOLR_CORE_URL).withConnectionTimeout(200000).withSocketTimeout(900000).build();
+    }
+
+
+
                                     runWithTimeout(new Runnable() {
                                         @Override
                                         public void run() {
@@ -40,4 +51,50 @@
                                             }
                                         }
                                     }, 60, TimeUnit.SECONDS); //400
+
+
+
+
+
+
+
+	    public void runWithTimeout(final Runnable runnable, long timeout, TimeUnit timeUnit) throws Exception {
+		    runWithTimeout(new Callable<Object>() {
+			    @Override
+			    public Object call() throws Exception {
+				    runnable.run();
+				    return null;
+			    }
+		    }, timeout, timeUnit);
+	    }
+
+	    public <T> T runWithTimeout(Callable<T> callable, long timeout, TimeUnit timeUnit) throws Exception {
+		    final ExecutorService executor = Executors.newSingleThreadExecutor();
+		    final Future<T> future = executor.submit(callable);
+		    executor.shutdown(); // This does not cancel the already-scheduled task.
+		    try {
+			    return future.get(timeout, timeUnit);
+		    }
+		    catch (TimeoutException e) {
+			    //remove this if you do not want to cancel the job in progress
+			    //or set the argument to 'false' if you do not want to interrupt the thread
+			    future.cancel(true);
+			    //throw e;
+                System.out.printf("Interrupted\n");
+			    return null;
+		    }
+		    catch (ExecutionException e) {
+			    //unwrap the root cause
+			    Throwable t = e.getCause();
+			    if (t instanceof Error) {
+				    throw (Error) t;
+			    } else if (t instanceof Exception) {
+				    throw (Exception) e;
+			    } else {
+				    throw new IllegalStateException(t);
+			    }
+		    }
+	    }
+    //}    
+
 
